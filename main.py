ad7e2e759deab48691b3a9ac554d33be8a5ebabe
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-31 18:18:10 +0100

    product  image

--- main.py
+++ main.py
@@ -161,6 +161,52 @@ async def create_upload_file(file: UploadFile = File(...),
     file_url = "localhost:8000"+generated_name[1:]
     return {"status": "ok", "filename": file_url}
 
+@app.post("/uploadfile/product/{id}")
+async def create_upload_file(id: int, file: UploadFile = File(...),
+                                user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = "./static/images/"
+    filename = file.filename
+    # test.png >> ["test", "png"]
+    extension = filename.split(".")[1]
+
+    if extension not in ["png", "jpg", "jpeg"]:
+        return {"status": "error", "details": "File extension not allowed"}
+   
+    #exemple /static/images/u3u59jlkjk.png
+    token_name = secrets.token_hex(10) +"."+extension
+    generated_name = FILEPATH +token_name
+    file_content = await file.read()
+
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+
+    #Pillow
+
+    img = Image.open(generated_name)
+    img = img.resize(size=(200,200))
+    img.save(generated_name)
+
+    file.close()
+
+    product = await Product.get(id =id)
+    business = await product.business
+    owner = await business.owner
+
+    if owner == user:
+        product.product_image = token_name
+        await product.save()
+    else:
+         raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            details ="not authenticated to perform this action ",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    file_url = "localhost:8000"+generated_name[1:]
+    return {"status": "ok", "filename": file_url} 
+
+
+
+
 register_tortoise(
     app,
     db_url="sqlite://database.sqlite3",
