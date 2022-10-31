18dbdf474e195ce5165d141da08509636c477914
HanaOuerghemmi <5004@holbertonschool.com>
2022-10-31 17:50:01 +0100

    profil image

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # ecomerceBackend
+
+pip install fastapi
+
+python3 -m venv fast-anv
+
+source fast-env/bin/activate
+
+pip3 install fastapi
+
+pip3 install uvicorn
+
+uvicorn --version
+
+Run:
+    uvicorn main:app --reload
+Pydantic swagger UI (text Api directory  we didint front end to test)
+
+pip install passlib[bcrypt]
+pip install tortoise
+
+mail:
+pip install fastapi-mail
+pipi install python --dotenv
+
+token
+pip install pyjwt
+
+pip install python-multipart
+images Pollow
+
+python3 -m pip install --upgrade PIllow
+
+AIOFiles
+pip install aiofiles
+create requirement
+pipi freeze > requirements.txt
--- main.py
+++ main.py
@@ -16,6 +16,12 @@ from dotenv import dotenv_values
 # templates
 from fastapi.templating import Jinja2Templates
 
+#image upload
+from fastapi import File, UploadFile
+import secrets
+from fastapi.staticfiles import StaticFiles
+from PIL import Image 
+
 # HTMLResponse
 from fastapi.responses import HTMLResponse
 
@@ -24,6 +30,10 @@ config_credentials = dotenv_values(".env")
 app = FastAPI()
 
 oath2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+#static files setup config
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.post('/token')
 async def generate_token(request_from: OAuth2PasswordRequestForm = Depends()):
@@ -105,11 +115,51 @@ async def email_verification(request: Request, token: str):
         headers={"WWW-Authenticate": "Bearer"},
     )
 
-
 @app.get("/")
 def index():
     return {"Message": "hey "}
 
+@app.post("/uploadfile/profile")
+async def create_upload_file(file: UploadFile = File(...),
+                                 user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = "./static/images/"
+    filename = file.filename
+    # test.png >> ["test", "png"]
+    extension = filename.split(".")[1]
+
+    if extension not in ["png", "jpg", "jpeg"]:
+        return {"status": "error", "details": "File extension not allowed"}
+   
+    #exemple /static/images/u3u59jlkjk.png
+    token_name = secrets.token_hex(10) +"."+extension
+    generated_name = FILEPATH +token_name
+    file_content = await file.read()
+
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+
+    #Pillow
+
+    img = Image.open(generated_name)
+    img = img.resize(size=(200,200))
+    img.save(generated_name)
+
+    file.close()
+
+    business =await Business.get(owner = user)
+    owner = await business.owner
+
+    if owner == user:
+        business.logo = token_name
+        await business.save()
+    else:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            details ="not authenticated to perform this action ",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    file_url = "localhost:8000"+generated_name[1:]
+    return {"status": "ok", "filename": file_url}
 
 register_tortoise(
     app,
